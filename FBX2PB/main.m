50e9d3376eaedfc9ecee43760ef190afa3d59944
Kunal Thacker <kunalt@hike.in>
2017-06-27 18:31:57 +0530

    Initial Commit

---
+++ FBX2PB/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FBX2PB
+//
+//  Created by Kunal Thacker on 27/06/17.
+//  Copyright © 2017 Kunal Thacker. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}
